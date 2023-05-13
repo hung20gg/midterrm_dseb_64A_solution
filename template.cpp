1d3a033c25ac24a61f621bac83ffe456bb2983ed
hung20gg <quanghung20gg@gmail.com>
2023-05-13 23:41:23 +0700

    Template để làm bài tập

---
+++ template.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Rút gọn câu lệnh
+typedef unsigned long long ll;
+typedef double ld;
+typedef unsigned long long ull;
+typedef long long int lli;
+
+#define faster() ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+
+void solve(){
+    /*
+    Chỉ cần code trong khu vực này thôi, không cần nhập biến t.
+    Nếu có gọi hàm thì nhớ để ra ngoài.
+    */
+   
+}
+int main(){
+    //buff thời gian
+    faster();
+    int test;
+    cin>>test;
+    for (int i=0;i<test;i++){
+        solve();
+    }
+}
